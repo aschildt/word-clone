45600cd92da11c7ad763d6beff559bac2f4d3610
Alex Schildt <alexandra.96schildt@hotmail.com>
2023-04-10 21:36:56 +0200

    4

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { range } from "../../utils";
+import { checkGuess } from "../../game-helpers";
 
-function Guess({string}) {
-  console.log('Guess', string)
+function Guess({string, answer}) {
+  const result = checkGuess(string, answer)
+
   return <p className="guess">
     {range(5).map(i => (
-      <span className="cell" key={i}>{string ? string[i] : undefined}</span>
+      <span className={"cell".concat(result ? (" ".concat(result[i].status)) : '')} key={i}>{result ? result[i].letter : undefined}</span>
     ))}
   </p>;
 }
--- src/components/RenderGuesses/RenderGuesses.js
+++ src/components/RenderGuesses/RenderGuesses.js
@@ -3,10 +3,10 @@ import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 import Guess from "../Guess";
 import { range } from "../../utils";
 
-function RenderGuesses({guesses}) {
+function RenderGuesses({guesses, answer}) {
   return <div>
     {range(NUM_OF_GUESSES_ALLOWED).map(i =>
-      <Guess key={i} string={guesses[i]}/>
+      <Guess key={i} string={guesses[i]} answer={answer}/>
     )}
   </div>
 }
